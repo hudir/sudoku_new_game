5162fd54320371f8e0b18f00054c91c9ce79fb07
Zhuo <hudirybw@gmail.com>
2023-03-14 15:08:24 +0100

    feat: update row

--- README.md
+++ README.md
@@ -29,12 +29,14 @@ You can use broswer with the url [http://localhost:5000/newgame](http://localhos
 
   #### 1- need a random fulfilled sudoku puzzle
   - 9x9 puzzle with numbers from 1-9
-     - at begining puzzle shold looks like [1-9].repeat(81).join() - array with 81 element, each ele is also a arr with 1-9 inside of it
-     - store the puzzle and take a random number from the first array, and replace the arr with it
+     - at begining puzzle shold looks like [1-9].repeat(81) - array with 81 element, each ele is also a arr with 1-9 inside of it
+     - store the puzzle and take a random number from the first array element, and replace the arr with it
      - update the line, row and region, all the array element should not have the random number anymore
      - repeat this process until all element in puzzle are numbers(no array ele left)
   - check each line, each number should only only appear only 1 time
   - check each row
   - check each region
    
-  #### 2- hide some element of it base on level(easy-10, middle-20)
+  #### 2- hide some element of it
+  - but make sure the puzzle still have and only have one unique solution
+         
--- controller/sudoku.js
+++ controller/sudoku.js
@@ -1,6 +1,48 @@
 class Sudoku{
     constructor(level){
         this.level = level
+        this.oneToNine = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        this.puzzle = this.createInitPuzzle()
+    };
+
+    createInitPuzzle(){
+        let arr = []
+        for(let i = 0; i < 81; i++)  arr.push(this.oneToNine)
+        return arr
     }
 
+    randomNum(arr){
+        return arr[Math.floor(Math.random() * arr.length)]
+    }
+
+    createNewFulfilledPuzzle(){
+        for(let i = 0; i < 81; i++){
+            const num = this.randomNum(this.puzzle[i])
+            this.puzzle[i] = num
+            this.updateRow(i, num)
+
+        }
+
+        return this.puzzle
+    }
+
+    updateRow(index, num){
+        const rowStart = Math.floor(index / 9) * 9
+        const rowEnd = rowStart + 8 
+        // console.log(rowStart, ' ', rowEnd)
+        for(let i = rowStart; i <= rowEnd; i++) {
+            if(Array.isArray(this.puzzle[i])){
+                this.puzzle[i] = this.puzzle[i].filter(x=> x != num)
+            }
+        }
+    }
+
+
+
 }
+
+const test = new Sudoku()
+console.log(test.puzzle.length) //==> 81
+console.log(test.updateRow(80)) 
+console.log(test.createNewFulfilledPuzzle())
+
